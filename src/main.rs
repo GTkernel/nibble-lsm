198843130185a66ae721d96f7e427512cf5889a7
Alexander Merritt <merritt.alex@gatech.edu>
2016-03-08 17:42:48 -0500

    Move memmap functions to module

--- src/main.rs
+++ src/main.rs
@@ -8,48 +8,19 @@ extern crate libc;
 extern crate log;
 
 mod logger;
+mod memutil;
 
 use std::rc::Rc;
 use std::cell::RefCell;
 
-/// Memory mapped region in our address space.
-struct MemMap {
-    addr: usize,
-    len: usize,
-}
-
-/// Create anonymous private memory mapped region.
-impl MemMap {
-    fn new(len: usize) -> Self {
-        // TODO fault on local socket
-        let prot: libc::c_int = libc::PROT_READ | libc::PROT_WRITE;
-        let flags: libc::c_int = libc::MAP_ANON |
-            libc::MAP_PRIVATE | libc::MAP_NORESERVE;
-        let addr: usize = unsafe {
-            let p = 0 as *mut libc::c_void;
-            libc::mmap(p, len, prot, flags, 0, 0) as usize
-        };
-        info!("mmap 0x{:x} {} MiB", addr, len>>20);
-        assert!(addr != libc::MAP_FAILED as usize);
-        MemMap { addr: addr, len: len }
-    }
-}
-
-/// Prevent dangling regions by unmapping it.
-impl Drop for MemMap {
-    fn drop(&mut self) {
-        unsafe {
-            let p = self.addr as *mut libc::c_void;
-            libc::munmap(p, self.len);
-        }
-    }
-}
+use memutil::MemMap;
 
 type BlockRef = Option<Rc<RefCell<Block>>>;
 
 struct Block {
     addr: usize,
     len: usize,
+    // TODO owning Segment
 }
 
 impl Block {
@@ -72,7 +43,7 @@ impl BlockAllocator {
         let count = bytes / block_size;
         let mut pool: Vec<Block> = Vec::new();
         for b in 0..count {
-            let addr = mmap.addr + b*block_size;
+            let addr = mmap.addr() + b*block_size;
             pool.push(Block::new(addr, block_size));
         }
         BlockAllocator { block_size: block_size,
@@ -98,7 +69,6 @@ fn main() {
 #[cfg(test)]
 mod test {
     use super::BlockAllocator;
-    use super::MemMap;
 
     #[test]
     fn block_allocator() {
@@ -108,15 +78,4 @@ mod test {
         let ba = BlockAllocator::new(block_size, alloc_size);
         //ba.alloc(1);
     }
-
-    #[test]
-    fn map() {
-        let len = 1<<26;
-        let mm = MemMap::new(len);
-        assert_eq!(mm.len, len);
-        assert!(mm.addr != 0 as usize);
-        // TODO touch the memory somehow
-        // TODO verify mmap region is unmapped
-    }
-
 }
