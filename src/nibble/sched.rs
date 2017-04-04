067374123d6d05842e0cd4721cf35a2835892f57
Alexander Merritt <merritt.alex@gatech.edu>
2017-04-03 18:39:08 -0700

    sched: add thread sleeping helper funcs

--- src/nibble/sched.rs
+++ src/nibble/sched.rs
@@ -1,7 +1,9 @@
 use libc;
 use std::mem;
 use std::thread;
+use std::time::{Duration};
 use nibble::numa;
+use nibble::common;
 
 // TODO make afunction that runs a closure while pinned. that way we
 // can set and then unset the cpu mask within the function itself...
@@ -62,5 +64,20 @@ pub unsafe fn pin_map<F>(cpu: usize, func: F) where F: Fn() {
     // restore mask
     ret = libc::sched_setaffinity(tid, size, &mut mask);
     assert_eq!(ret, 0);
+}
+
+/// Sleep the calling thread for a random amount of time bounded above
+/// and below by low_ms and high_ms.
+pub fn sleep_rand(low_ms: usize, high_ms: usize) {
+    debug_assert!(high_ms > low_ms);
+    let d = high_ms - low_ms;
+    let t = unsafe { common::rdrandq() % d as u64 } + low_ms as u64;
+    let dur = Duration::from_millis(t);
+    thread::sleep(dur);
+}
+
+/// Helper function to sleep a random amount of time, sub-second.
+pub fn sleep_short() {
+    sleep_rand(200, 500);
 }
 
