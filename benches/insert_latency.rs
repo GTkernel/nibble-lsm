df04bfe11b2a5c2b8dbccbc663ced710ac32eb26
Alexander Merritt <merritt.alex@gatech.edu>
2017-04-26 14:27:20 -0400

    updated a bench file

--- benches/insert_latency.rs
+++ benches/insert_latency.rs
@@ -7,12 +7,12 @@
 
 #![feature(test)]
 
-/// Specify amount of memory in GiB Nibble should allocate for the
+/// Specify amount of memory in GiB LSM should allocate for the
 /// logs.  The index will acquire more memory for the hash tables.
-const NIBBLE_CAPACITY: usize = 30;
+const LSM_CAPACITY: usize = 30;
 
 extern crate test;
-extern crate nibble;
+extern crate kvs;
 extern crate rand;
 
 #[macro_use]
@@ -24,12 +24,12 @@ use test::Bencher;
 use std::mem;
 use std::sync::{Once, ONCE_INIT};
 
-use nibble::common::Pointer;
-use nibble::nib::{Nibble,PutPolicy};
-use nibble::segment::ObjDesc;
-use nibble::sched::pin_cpu;
-use nibble::logger;
-use nibble::numa::{self,NodeId};
+use kvs::common::Pointer;
+use kvs::lsm::{LSM,PutPolicy};
+use kvs::segment::ObjDesc;
+use kvs::sched::pin_cpu;
+use kvs::logger;
+use kvs::numa::{self,NodeId};
 
 // TODO Put unique objects (will not perform atomic decrement of old
 // segment).
@@ -46,8 +46,8 @@ type KeyType = u64;
 // bench iterations. Each outer function is called many times, as well
 // as each invocation of Bencher::iter.
 lazy_static! {
-    pub static ref NIBBLE: Nibble =
-        Nibble::new(NIBBLE_CAPACITY<<30);
+    pub static ref KVS: LSM =
+        LSM::new(LSM_CAPACITY<<30);
 }
 
 static START: Once = ONCE_INIT;
@@ -59,29 +59,29 @@ fn setup() {
         unsafe { pin_cpu(0); }
         //let nsockets = numa::NODE_MAP.sockets();
         //for s in 0..nsockets {
-        //    NIBBLE.enable_compaction(NodeId(s));
+        //    KVS.enable_compaction(NodeId(s));
         //}
     });
 }
 
 fn insert_n(b: &mut Bencher, p: PutPolicy,
-            nib: &Nibble, len: usize) {
+            kvs: &LSM, len: usize) {
     let mut rng = rand::thread_rng();
     let key = rng.gen::<u64>();
     let val: Vec<u8> = Vec::with_capacity(len);
     let obj = ObjDesc::new(key, Pointer(val.as_ptr()), len);
-    b.iter( || { nib.put_where(&obj,p) });
+    b.iter( || { kvs.put_where(&obj,p) });
 }
 
-fn local_insert_n(b: &mut Bencher, nib: &Nibble, len: usize) {
+fn local_insert_n(b: &mut Bencher, kvs: &LSM, len: usize) {
     let policy = PutPolicy::Specific(0);
-    insert_n(b, policy, nib, len);
+    insert_n(b, policy, kvs, len);
 }
 
-fn remote_insert_n(b: &mut Bencher, nib: &Nibble, len: usize) {
+fn remote_insert_n(b: &mut Bencher, kvs: &LSM, len: usize) {
     let node = numa::NODE_MAP.sockets() - 1;
     let policy = PutPolicy::Specific(node);
-    insert_n(b, policy, nib, len);
+    insert_n(b, policy, kvs, len);
 }
 
 // ---- Local tests ----
@@ -90,63 +90,63 @@ fn remote_insert_n(b: &mut Bencher, nib: &Nibble, len: usize) {
 fn local_insert_16(b: &mut Bencher) {
     setup();
     let len = 16_usize - mem::size_of::<KeyType>();
-    local_insert_n(b, &NIBBLE, len);
+    local_insert_n(b, &KVS, len);
 }
 
 #[bench]
 fn local_insert_30(b: &mut Bencher) {
     setup();
     let len = 30_usize - mem::size_of::<KeyType>();
-    local_insert_n(b, &NIBBLE, len);
+    local_insert_n(b, &KVS, len);
 }
 
 #[bench]
 fn local_insert_60(b: &mut Bencher) {
     setup();
     let len = 60_usize - mem::size_of::<KeyType>();
-    local_insert_n(b, &NIBBLE, len);
+    local_insert_n(b, &KVS, len);
 }
 
 #[bench]
 fn local_insert_100(b: &mut Bencher) {
     setup();
     let len = 100_usize - mem::size_of::<KeyType>();
-    local_insert_n(b, &NIBBLE, len);
+    local_insert_n(b, &KVS, len);
 }
 
 #[bench]
 fn local_insert_200(b: &mut Bencher) {
     setup();
     let len = 200_usize - mem::size_of::<KeyType>();
-    local_insert_n(b, &NIBBLE, len);
+    local_insert_n(b, &KVS, len);
 }
 
 #[bench]
 fn local_insert_500(b: &mut Bencher) {
     setup();
     let len = 500_usize - mem::size_of::<KeyType>();
-    local_insert_n(b, &NIBBLE, len);
+    local_insert_n(b, &KVS, len);
 }
 
 #[bench]
 fn local_insert_1000(b: &mut Bencher) {
     setup();
     let len = 1000_usize - mem::size_of::<KeyType>();
-    local_insert_n(b, &NIBBLE, len);
+    local_insert_n(b, &KVS, len);
 }
 
 #[bench]
 fn local_insert_2000(b: &mut Bencher) {
     setup();
     let len = 3000_usize - mem::size_of::<KeyType>();
-    local_insert_n(b, &NIBBLE, len);
+    local_insert_n(b, &KVS, len);
 }
 
 #[bench]
 fn local_insert_4000(b: &mut Bencher) {
     setup();
     let len = 4000_usize - mem::size_of::<KeyType>();
-    local_insert_n(b, &NIBBLE, len);
+    local_insert_n(b, &KVS, len);
 }
 
 // ---- Distant tests ----
@@ -155,62 +155,62 @@ fn local_insert_4000(b: &mut Bencher) {
 fn remote_insert_16(b: &mut Bencher) {
     setup();
     let len = 16_usize - mem::size_of::<KeyType>();
-    remote_insert_n(b, &NIBBLE, len);
+    remote_insert_n(b, &KVS, len);
 }
 
 #[bench]
 fn remote_insert_30(b: &mut Bencher) {
     setup();
     let len = 30_usize - mem::size_of::<KeyType>();
-    remote_insert_n(b, &NIBBLE, len);
+    remote_insert_n(b, &KVS, len);
 }
 
 #[bench]
 fn remote_insert_60(b: &mut Bencher) {
     setup();
     let len = 60_usize - mem::size_of::<KeyType>();
-    remote_insert_n(b, &NIBBLE, len);
+    remote_insert_n(b, &KVS, len);
 }
 
 #[bench]
 fn remote_insert_100(b: &mut Bencher) {
     setup();
     let len = 100_usize - mem::size_of::<KeyType>();
-    remote_insert_n(b, &NIBBLE, len);
+    remote_insert_n(b, &KVS, len);
 }
 
 #[bench]
 fn remote_insert_200(b: &mut Bencher) {
     setup();
     let len = 200_usize - mem::size_of::<KeyType>();
-    remote_insert_n(b, &NIBBLE, len);
+    remote_insert_n(b, &KVS, len);
 }
 
 #[bench]
 fn remote_insert_500(b: &mut Bencher) {
     setup();
     let len = 500_usize - mem::size_of::<KeyType>();
-    remote_insert_n(b, &NIBBLE, len);
+    remote_insert_n(b, &KVS, len);
 }
 
 #[bench]
 fn remote_insert_1000(b: &mut Bencher) {
     setup();
     let len = 1000_usize - mem::size_of::<KeyType>();
-    remote_insert_n(b, &NIBBLE, len);
+    remote_insert_n(b, &KVS, len);
 }
 
 #[bench]
 fn remote_insert_2000(b: &mut Bencher) {
     setup();
     let len = 3000_usize - mem::size_of::<KeyType>();
-    remote_insert_n(b, &NIBBLE, len);
+    remote_insert_n(b, &KVS, len);
 }
 
 #[bench]
 fn remote_insert_4000(b: &mut Bencher) {
     setup();
     let len = 4000_usize - mem::size_of::<KeyType>();
-    remote_insert_n(b, &NIBBLE, len);
+    remote_insert_n(b, &KVS, len);
 }
 
